8c4218896d328a06a7042b9e2191ff8dfab3364d
Moulik24 <moulikapmehta@gmail.com>
2023-04-08 13:49:15 -0500

    Allows user to specify an input word or to type EXIT, and gives corresponding word. Does this in a loop.

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include "fomalib.h"
 
 struct fsm *HVD() {
@@ -11,12 +12,27 @@ int main() {
     struct fsm *HVD_fst = HVD();
     struct apply_handle *ah = apply_init(HVD_fst);
     ah = apply_init(HVD_fst);
+    
+    printf("\nWelcome! Enter your word after the prompt \"Please enter your word\". When you want to stop the program, type EXIT after this prompt.\n\n");
+    
+    char input_word[20];
+    while(1) {
+        printf("Please enter your word: \n");
+        scanf("%s", input_word);
+        if(strcmp(input_word,"EXIT") == 0) {
+            printf("Thank you!\n");
+            break;
+        }
 
-    char *result = apply_down(ah, "tht");
-    while (result != NULL) {
-        printf("%s\n", result);
-        result = apply_down(ah, NULL);
-    }
+        char *result = apply_down(ah, input_word);
+        if(result == NULL) {
+            printf("Sorry, that is not a word in my dictionary. Please try another word.\n\n");
+        }
+        while (result != NULL) {
+            printf("Resulting word is: \n%s \n\n", result);
+            result = apply_down(ah, NULL);
+        }
+    }    
 
     apply_clear(ah); 
     fsm_destroy(HVD_fst);

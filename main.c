86abaf5dee393dbe6f6248196d2bc956e7deaec8
Moulik24 <moulikapmehta@gmail.com>
2023-04-09 18:32:15 -0500

    Go from any romanized sound in Japanese to the feature category {t,d,l,h}. Then, apply HVD. Haven't implemented going back to a romanized representation yet, though.

--- main.c
+++ main.c
@@ -1,17 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "fomalib.h"
 
+struct fsm *convert_to_categories() {
+    struct fsm *net;
+    net = fsm_parse_regex("[a:l|i:h|u:h|e:l|o:l|k:t|g:d|s:t|z:d|t:t|d:d|ts:t|ch:t|j:d|dz:d|n:d|h:t|f:t|b:d|p:t|m:d|y:d|r:d|w:d]*", NULL, NULL);
+    return net;
+}
+
 struct fsm *HVD() {
     struct fsm *net;
-    net = fsm_parse_regex("[t:t|d:d|l:l|h:h]* .o. h -> o || t _ t .o. h -> o || t _ .#.", NULL, NULL); 
+    net = fsm_parse_regex("h -> o || t _ t .o. h -> o || t _ .#.", NULL, NULL); 
     return net;
 }
 
 int main() {
+    struct fsm *feature_abstraction = convert_to_categories();
     struct fsm *HVD_fst = HVD();
-    struct apply_handle *ah = apply_init(HVD_fst);
-    ah = apply_init(HVD_fst);
+    struct fsm *combined = fsm_compose(feature_abstraction, HVD_fst);
+
+    struct apply_handle *ah = apply_init(combined);
+    ah = apply_init(combined);
     
     printf("\nWelcome! Enter your word after the prompt \"Please enter your word\". When you want to stop the program, type EXIT after this prompt.\n\n");
     
@@ -32,9 +42,10 @@ int main() {
             printf("Resulting word is: \n%s \n\n", result);
             result = apply_down(ah, NULL);
         }
+        free(result);
     }    
 
     apply_clear(ah); 
-    fsm_destroy(HVD_fst);
+    fsm_destroy(combined);
     return 0;
 }
